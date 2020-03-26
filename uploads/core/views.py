741e0139d4f2e1ca35be5e5a48e8d725bab7bc2f
Gordon Xu <gordonxyh@gmail.com>
2020-03-26 14:29:08 -0400

    test_version

---
+++ uploads/core/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+from uploads.core.models import Document
+from uploads.core.forms import DocumentForm
+
+
+def home(request):
+    documents = Document.objects.all()
+    return render(request, 'core/home.html', { 'documents': documents })
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+
+        myfile = request.FILES['myfile']
+        query = request.POST['request']
+        print(query)
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url,
+            'output_file_url':uploaded_file_url
+        })
+
+    return render(request, 'core/simple_upload.html')
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            myfile = request.FILES['document']
+            query = request.POST['request']
+            print(query)
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(filename)
+            return render(request, 'core/model_form_upload.html', {
+                'uploaded_file_url': uploaded_file_url,
+                'output_file_url':uploaded_file_url
+            })
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
+
+# def model_form_upload(request):
+#     if request.method == 'POST':
+#         form = DocumentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = DocumentForm()
+#     return render(request, 'core/model_form_upload.html', {
+#         'form': form
+#     })
