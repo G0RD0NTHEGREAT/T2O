67c7ab302b0f36ef9ba93353fa8e1a48655231a4
jshi31 <j.shi@rochester.edu>
2020-06-15 22:01:43 -0400

    safety

--- run_remote_single.sh
+++ run_remote_single.sh
@@ -7,8 +7,8 @@ demooutdir='/u/jshi31/project/LDIE/output/FiveK_trial_1/demo_output'
 input_path=$demoindir/$filenamebase64
 output_path="$demooutdir/$filenamebase64.jpg"
 # upload the image
-scp $1 yxu74@cornea.cs.rochester.edu:$input_path
+scp $1 jshi31@cornea.cs.rochester.edu:$input_path
 # call model
-ssh yxu74@cornea.cs.rochester.edu /u/jshi31/project/LDIE/run_demo_FiveK.sh $input_path 0 $2
+ssh jshi31@cornea.cs.rochester.edu /u/jshi31/project/LDIE/run_demo_FiveK.sh $input_path 0 $2
 # download the image
-scp yxu74@cornea.cs.rochester.edu:$output_path $3
+scp jshi31@cornea.cs.rochester.edu:$output_path $3
